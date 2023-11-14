4fe1d4dfe304cd222442b8e348e197949ea8888c
p-kino <p-kino@p-kino.com>
2023-11-14 11:34:47 +0900

    feat: Markdownのインデント幅を設定

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -16,3 +16,12 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+  end
+})
+
