2e1d2d9cbe45183dee61025c346b8265c78f451a
p-kino <p-kino@p-kino.com>
2024-01-13 21:28:07 +0900

    feat: neovimの背景を透明化

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_set_option('termguicolors', true)
+vim.api.nvim_set_option('winblend', 0)
+vim.api.nvim_set_option('pumblend', 0)
 vim.api.nvim_set_option('scrolloff', 5)
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
 
