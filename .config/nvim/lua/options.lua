aa8684980bb81a10937fafe8a331f0b00654c7d2
p-kino <p-kino@p-kino.com>
2024-01-25 18:23:37 +0900

    change: showmodeをfalseにした
    lualine.nvimにもmodeが表示されるので

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,4 +3,5 @@ vim.api.nvim_set_option('winblend', 0)
 vim.api.nvim_set_option('pumblend', 0)
 vim.api.nvim_set_option('scrolloff', 5)
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
+vim.o.showmode = false
 
