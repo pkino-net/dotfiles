f2d4a4dae30f73a4925c10663997f4c325cf9eb3
p-kino <p-kino@p-kino.com>
2024-01-26 15:51:28 +0900

    fix: nvim-cmpの設定漏れを修正

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,6 +2,8 @@ require('mason').setup()
 
 require("mason-lspconfig").setup_handlers ({
   function(server)
-    require("lspconfig")[server].setup({})
+    require("lspconfig")[server].setup({
+      capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    })
   end
 })
