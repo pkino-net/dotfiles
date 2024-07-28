1d1a849fb353c5b14a12feb3f5c96ac10f384cc9
pkinop <75358673+pkinop@users.noreply.github.com>
2024-07-28 22:21:21 +0900

    add solargraph config

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,21 @@
 require('mason').setup()
 
-require("mason-lspconfig").setup_handlers ({
+local mason_lspconfig = require("mason-lspconfig")
+local servers = {
+  ruby_lsp = {},
+}
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers ({
   function(server)
     require("lspconfig")[server].setup({
       capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      on_attach = on_attach,
+      settings = servers[server],
+      filetypes = (servers[server] or {}).filetypes,
     })
   end
 })
